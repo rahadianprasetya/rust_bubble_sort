ae653124dc2b37066b4238c448abc58656d8c9f4
rahadian.prast <rahadian.prast@gmail.com>
2026-05-26 01:22:36 +0700

    add psedo random

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::{fmt::Debug, vec};
+use std::fmt::Debug;
+mod b_rand;
 
 // O(n^2) BIG O is n square
 pub fn bubble_short<T: PartialOrd + Debug>(v: &mut [T]) {
@@ -66,6 +67,39 @@ pub fn merge_sort<T: PartialOrd + Debug>(mut v: Vec<T>) -> Vec<T> {
     }
 }
 
+// Move first element to the correct place
+// Everything lower should be before it,
+// Everything higher should be after if,
+// return it's location
+
+pub fn pivot<T: PartialOrd>(v: &mut [T]) -> usize {
+    let mut p = b_rand::rand(v.len());
+    v.swap(p, 0);
+    p = 0;
+
+    for i in 1..v.len() {
+        if v[i] < v[p] {
+            // move our pivot forward + 1, and puts this element before it
+            v.swap(p + 1, i);
+            v.swap(p, p + 1);
+            p += 1;
+        }
+    }
+    p
+}
+
+pub fn quick_sort<T: PartialOrd + Debug>(v: &mut [T]) {
+    if v.len() <= 1 {
+        return;
+    }
+    let p = pivot(v);
+    println!("{:?}", v);
+
+    let (a, b) = v.split_at_mut(p);
+    quick_sort(a);
+    quick_sort(&mut b[1..]);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -84,5 +118,27 @@ mod tests {
         let v = merge_sort(v);
         println!("{:?}", v);
         assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
+    }
+
+    #[test]
+    fn test_pivot() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        let p = pivot(&mut v);
+        println!("{:?}", p);
+
+        for x in 0..v.len() {
+            assert!((v[x] < v[p]) == (x < p));
+        }
+
+        //assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13])
+    }
+
+    #[test]
+    fn test_quick_sort() {
+        let mut v = vec![4, 6, 1, 8, 11, 13, 3];
+        quick_sort(&mut v);
+        println!("{:?}", v);
+        assert_eq!(v, vec![1, 3, 4, 6, 8, 11, 13]);
+        panic!();
     }
 }
